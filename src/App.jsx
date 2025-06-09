9095b097ae42b64bcea308f20809edab59eadd36
Jules-Elvin Andrade <juleselvandrade@gmail.com>
2025-06-09 16:43:55 +0900

    flexbox limit testing works :)

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Card from "./components/Card";
 const App = () => {
 
   return (
-    <div> 
-      <h1> THE TITLE </h1>
-      <h2> the subtitle </h2>
-      <Card image="bazin" title="ga1" desc="bazing" out="https://www.youtube.com/watch?v=1mXwnL2T_es" />
-      <Card image="bazin" title="ga1" desc="bazing" out="https://www.youtube.com/watch?v=1mXwnL2T_es" />
-      <Card image="bazin" title="ga1" desc="bazing" out="https://www.youtube.com/watch?v=1mXwnL2T_es" />
-      <Card image="bazin" title="ga1" desc="bazing" out="https://www.youtube.com/watch?v=1mXwnL2T_es" />
-    </div>
+    <>
+      <h1 className="fronthead"> THE TITLE </h1>
+      <h2 className="subhead"> the subtitle </h2>
+        <div className="page-container">
+          <Card image="bazin" title="ga1" desc="bazing" out="https://www.youtube.com/watch?v=1mXwnL2T_es" />
+          <Card image="bazin" title="ga1" desc="bazing" out="https://www.youtube.com/watch?v=1mXwnL2T_es" />
+          <Card image="bazin" title="ga1" desc="bazing" out="https://www.youtube.com/watch?v=1mXwnL2T_es" />
+          <Card image="bazin" title="ga1" desc="bazing" out="https://www.youtube.com/watch?v=1mXwnL2T_es" />
+        </div>
+    </>
 
   )
 }
